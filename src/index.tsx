54fcbf192ce4340c4dd6e85638e7547405460db6
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2024-10-08 12:50:54 +0300

    добавление сайдбара

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,22 +13,30 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	return (
-		<div
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
-			<Article />
-		</div>
-	);
+
+	  //состояние для хранения параметров статьи
+	  const [articleOptions, setArticleOptions] = useState<ArticleStateType>(defaultArticleState);
+
+	  // функция для обновления опций
+	  const setAppOptions = (value: ArticleStateType) => {
+		  setArticleOptions(value);
+	  };
+	  return (
+        <div
+            className={clsx(styles.main)}
+            style={
+                {
+                    '--font-family': articleOptions.fontFamilyOption.value,
+                    '--font-size': articleOptions.fontSizeOption.value,
+                    '--font-color': articleOptions.fontColor.value,
+                    '--container-width': articleOptions.contentWidth.value,
+                    '--bg-color': articleOptions.backgroundColor.value,
+                } as CSSProperties
+            }>
+            <ArticleParamsForm setAppOptions={setAppOptions} />
+            <Article />
+        </div>
+    );
 };
 
 root.render(
